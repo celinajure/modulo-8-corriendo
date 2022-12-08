781631c6727c86fac8bd6fcd66ab607221de707c
Celina <celinajure@gmail.com>
2022-12-08 15:42:26 -0300

    Segundo_intento

--- main/java/com/Celi/Springboot/entity/PersonaEntity.java
+++ main/java/com/Celi/Springboot/entity/PersonaEntity.java
@@ -22,6 +22,7 @@ public class PersonaEntity {
     private String descripcion;
     private String titulo;
     private String anio;
+    // aqui tratando de probar un git con 2 palabras
 
     public PersonaEntity() {
     }
